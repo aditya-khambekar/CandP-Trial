a2d7f483594b416bec47d4319ab6f7398c235589
Aditya <adityakhambekar9@gmail.com>
2024-08-27 23:23:40 -0500

    Created JFrame

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
-public class App{
+import javax.swing.JFrame;
+
+public class App extends JFrame{
     public static void main(String[]args){
-        System.out.println("Hello World!");
+        App app = new App();
+        app.setSize(1600, 900);
+        app.setVisible(true);
+
+        app.setName("Generic"); //TODO: change obviously
     }
 }
