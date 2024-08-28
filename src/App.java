777a59c5327bd8337aaf1b7012dcf61c7a261482
Aditya <adityakhambekar9@gmail.com>
2024-08-27 23:28:59 -0500

    Fixed screen name bug

--- src/App.java
+++ src/App.java
@@ -1,11 +1,13 @@
 import javax.swing.JFrame;
 
 public class App extends JFrame{
+    public App(String framename) {
+        super(framename);
+    }
+
     public static void main(String[]args){
-        App app = new App();
+        App app = new App("Generic");
         app.setSize(1600, 900);
         app.setVisible(true);
-
-        app.setName("Generic"); //TODO: change obviously
     }
 }
